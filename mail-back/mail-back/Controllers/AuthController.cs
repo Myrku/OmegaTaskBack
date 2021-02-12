b43dd44e5d8ee2a6adb3bab87a50cd26112dc85e
Myrku <DenisV_1@mail.ru>
2021-02-13 00:20:20 +0300

    Add jwt token

---
+++ mail-back/mail-back/Controllers/AuthController.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
+using System.Linq;
+using System.Security.Claims;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+using mail_back.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Options;
+using Microsoft.IdentityModel.Tokens;
+
+namespace mail_back.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthController : ControllerBase
+    {
+        private IOptions<AuthOptions> authOptions;
+        public AuthController(IOptions<AuthOptions> authoptions)
+        {
+            authOptions = authoptions;
+        }
+        List<User> users = new List<User>()
+        {
+            new User() {id = 1, username = "test22", email="test@maul.ru", password="qwerty1234", role="admin"}
+        };
+
+        [HttpPost]
+        [Route("sign-in")]
+        public IActionResult Login([FromBody] User user_req)
+        {
+            var user = AuthUser(user_req.username, user_req.password);
+            if (user != null)
+            {
+                var token = GenerateJWTToken(user);
+                return Ok(new
+                {
+                    access_token = token
+                });
+            }
+
+            return Unauthorized();
+        }
+
+        private User AuthUser(string username, string password)
+        {
+            return users.SingleOrDefault(u => u.username == username && u.password == password);
+        }
+        private string GenerateJWTToken(User user)
+        {
+            var authParam = authOptions.Value;
+            var securityKey = authParam.GetSymmetricSecurityKey();
+            var credentails = new SigningCredentials(securityKey, SecurityAlgorithms.HmacSha256);
+            var claims = new List<Claim>()
+            {
+                new Claim(JwtRegisteredClaimNames.UniqueName, user.username),
+                new Claim(JwtRegisteredClaimNames.Sub, user.id.ToString()),
+            };
+            var token = new JwtSecurityToken(authParam.Issuer, authParam.Audience, claims,
+                expires: DateTime.Now.AddSeconds(authParam.TokenLifetime), signingCredentials: credentails);
+            return new JwtSecurityTokenHandler().WriteToken(token);
+        }
+        private string GetHashPassword(string password)
+        {
+            using (SHA512 sha512 = SHA512.Create())
+            {
+                byte[] sourceBytes = Encoding.UTF8.GetBytes(password);
+                byte[] hashBytes = sha512.ComputeHash(sourceBytes);
+                string hash = BitConverter.ToString(hashBytes);
+                return hash;
+            }
+        }
+    }
+}
---
+++ mail-back/mail-back/Startup.cs
@@ -0,0 +1,86 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using mail_back.Models;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using Microsoft.IdentityModel.Tokens;
+
+namespace mail_back
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddControllers();
+            var authoptions = Configuration.GetSection("Auth").Get<AuthOptions>();
+            services.Configure<AuthOptions>(Configuration.GetSection("Auth"));
+
+            services.AddCors(options =>
+            {
+                options.AddDefaultPolicy(builder =>
+                {
+                    builder.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader();
+                });
+            });
+
+            services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme).AddJwtBearer(options =>
+            {
+                options.RequireHttpsMetadata = true;
+                options.TokenValidationParameters = new TokenValidationParameters
+                {
+                    ValidateIssuer = true,
+                    ValidIssuer = authoptions.Issuer,
+
+                    ValidateAudience = true,
+                    ValidAudience = authoptions.Audience,
+
+                    ValidateLifetime = true,
+
+                    IssuerSigningKey = authoptions.GetSymmetricSecurityKey(),
+                    ValidateIssuerSigningKey = true,
+
+                };
+            });
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseHttpsRedirection();
+            app.UseStaticFiles();
+
+            app.UseRouting();
+            app.UseCors();
+
+            app.UseAuthentication();
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+        }
+    }
+}
